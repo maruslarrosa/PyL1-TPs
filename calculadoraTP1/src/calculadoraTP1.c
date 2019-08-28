48c68a915e32603b38a4485bde7289da15b350d1
alumno <alumno@lab6pc00>
2019-08-28 19:10:33 -0300

    Agrego funcion para pedir numeros

--- calculadoraTP1/src/calculadoraTP1.c
+++ calculadoraTP1/src/calculadoraTP1.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : calculadoraTP1.c
- Author      : 
+ Author      : Marilina Larrosa
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -11,7 +11,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int getNumber( int *number1, int *number2);
+
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+	int number1, number2;
+	printf("Bienvenide a la calculadora.\n");
+	getNumber(&number1, &number2);
+}
+
+int getNumber( int *number1, int *number2){
+	printf("Por favor, ingrese el primer operando: ");
+	scanf("%d", &number1);
+	printf("Por favor, ingrese el segundo operando: ");
+	scanf("%d", &number2);
+	if(number1 && number2){
+		return 0;
+	}
+	return -1;
 }
