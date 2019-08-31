182c15041544b7e2dc3746c4cb94eea3254db78a
marularrosa <larrosa.marilina@gmail.com>
2019-08-31 17:05:33 -0400

    Valido ingreso de datos y cambio tipo de dato a enteros

--- calculadoraTP1/src/calculadoraTP1.c
+++ calculadoraTP1/src/calculadoraTP1.c
@@ -13,71 +13,93 @@
 #include <ctype.h>
 #include "operations.h"
 
-int getNumber( float *number1, float *number2);
-int showMenu(float number1, float number2, char *operator);
-void runOperation(float number1, float number2, char operator);
+#define SUCCESS 0
+#define ERROR -1
+
+int getNumber( int *number1, int *number2);
+int showMenu(int number1, int number2, char *operator);
+void runOperation(int number1, int number2, char operator);
 
 
 int main(void) {
 
-	float number1, number2;
+	int number1, number2;
 	char operator;
 	printf("Bienvenide a la calculadora.\n");
-	getNumber(&number1, &number2);
-
-	while (operator != 'S') {
-		showMenu(number1, number2, &operator);
-		runOperation(number1, number2, operator);
+	if (getNumber(&number1, &number2) == SUCCESS){
+		while (operator != 'S') {
+			if (showMenu(number1, number2, &operator) == SUCCESS){
+				runOperation(number1, number2, operator);
+			} else {
+				printf("Ocurrió un error al realizar la operación.\n");
+			}
+		}
+	} else {
+		printf("Ocurrió un error al solicitar los números.\n");
 	}
 	printf("Adiós");
 }
 
-int getNumber( float *number1, float *number2){
+int getNumber( int *number1, int *number2){
+	int status = -1;
+	int _number1, _number2;
 	printf("Por favor, ingrese el primer operando: ");
-	scanf("%f", number1);
-	printf("Por favor, ingrese el segundo operando: ");
-	scanf("%f", number2);
-	if(number1 && number2){
-		return 0;
+	while (scanf("%d", &_number1) != 1){
+		__fpurge(stdin);
+		printf("Valor Incorrecto.\nPor favor, ingrese el primer operando: ");
 	}
-	return -1;
+	printf("Por favor, ingrese el segundo operando: ");
+	while (scanf("%d", &_number2) != 1){
+		__fpurge(stdin);
+		printf("Valor Incorrecto.\nPor favor, ingrese el primer operando: ");
+	}
+	if(_number1 && _number2){
+		number1 = &_number1;
+		number2 = &_number2;
+		status = 0;
+	}
+	return status;
 }
 
-int showMenu(float number1, float number2, char *operator){
-	printf("Los números seleccionados son: %f y %f \n", number1, number2);
+int showMenu(int number1, int number2, char *operator){
+	int status = -1;
+	char _operator;
+	printf("Los números seleccionados son: %d y %d \n", number1, number2);
 	printf("Ingrese un operador (+ - * / !) o S para salir: \n");
 	__fpurge(stdin);
-	scanf("%c", operator);
-	if(operator) {
-		return 0;
+	scanf("%c", &_operator);
+	if(_operator) {
+		operator = &_operator;
+		status = 0;
 	}
-	return -1;
+	return status;
 }
 
-void runOperation(float number1, float number2, char operator){
-	float result;
+void runOperation(int number1, int number2, char operator){
+	int intResult;
+	float floatResult;
 	switch(operator){
 	case('+'):
-		sumNumbers(number1, number2, &result);
-		printf("El resultado de %f + %f es: %f \n",number1, number2, result);
+		sumNumbers(number1, number2, &intResult);
+		printf("El resultado de %d + %d es: %d \n",number1, number2, intResult);
 		break;
 	case('-'):
-		restNumbers(number1, number2, &result);
-		printf("El resultado de %f - %f es: %f \n",number1, number2, result);
+		restNumbers(number1, number2, &intResult);
+		printf("El resultado de %d - %d es: %d \n",number1, number2, intResult);
 		break;
 	case('*'):
-		multiplyNumbers(number1, number2, &result);
-		printf("El resultado de %f * %f es: %f \n",number1, number2, result);
+		multiplyNumbers(number1, number2, &intResult);
+		printf("El resultado de %d * %d es: %d \n",number1, number2, intResult);
 		break;
 	case('/'):
-		if (divideNumbers(number1, number2, &result) == 0){
-			printf("El resultado de %f / %f es: %f \n",number1, number2, result);
+		if (divideNumbers(number1, number2, &floatResult) == 0){
+			printf("El resultado de %d / %d es: %f \n",number1, number2, floatResult);
 		} else {
 			printf("No es posible dividir por cero.\n");
 		}
 		break;
 	case('!'):
-		sumNumbers(number1, number2, &result);
+		sumNumbers(number1, number2, &intResult);
 		break;
 	default:
 		break;
