1595f0b8e95689093628f83ff5e31b1646710c97
alumno <alumno@lab6pc00>
2019-09-04 22:10:12 -0300

    Cambio retorno de operations.c

--- calculadoraTP1/src/operations.c
+++ calculadoraTP1/src/operations.c
@@ -4,11 +4,9 @@
 
 
 int sumNumbers(int number1, int number2){
-	//int result;
 	int returnValue = -1;
 	int validNumbers = (number1*2/2) == number1 && (number2*2/2) == number2;
 	if(validNumbers){
-		//result = number1 + number2;
 		printf("El resultado de %d + %d es: %d \n\n",number1, number2, number1+number2);
 		returnValue = 0;
 	}
@@ -16,50 +14,50 @@ int sumNumbers(int number1, int number2){
 }
 
 int restNumbers(int number1, int number2){
-	int result;
-	if(number1 && number2){
-		result = number1 - number2;
-		printf("El resultado de %d - %d es: %d \n\n",number1, number2, result);
-		return 0;
+	int returnValue = -1;
+	int validNumbers = (number1*2/2) == number1 && (number2*2/2) == number2;
+	if(validNumbers){
+		printf("El resultado de %d - %d es: %d \n\n",number1, number2, number1 - number2);
+		returnValue = 0;
 	}
-	return -1;
+	return returnValue;
 }
 
 int divideNumbers(int number1, int number2){
-	float result;
-	if(number1 && number2 && number2 != 0){
-		result = (double)number1 / (double)number2;
+	int returnValue = -1;
+	int validNumbers = (number1*2/2) == number1 && (number2*2/2) == number2;
+	if(validNumbers && number2 != 0){
+		float result = (double)number1 / (double)number2;
 		printf("El resultado de %d / %d es: %f \n\n",number1, number2, result);
-		return 0;
+		returnValue = 0;
 	} else {
 		printf("No es posible dividir por cero.\n\n");
 	}
-	return -1;
+	return returnValue;
 }
 
 int multiplyNumbers(int number1, int number2){
-	int result;
-	if(number1 && number2){
-		result = number1 * number2;
-		printf("El resultado de %d * %d es: %d \n\n",number1, number2, result);
-		return 0;
+	int returnValue = -1;
+	int validNumbers = (number1*2/2) == number1 && (number2*2/2) == number2;
+	if(validNumbers){
+		printf("El resultado de %d * %d es: %d \n\n",number1, number2, number1 * number2);
+		returnValue = 0;
 	}
-	return -1;
+	return returnValue;
 }
 
 int numberFactorial(int number){
-	if(number){
+	int returnValue = -1;
+	int validNumber = (number*2/2) == number;
+	if(validNumber){
 		int i;
 		int factorial = 1;
 		for(i = 1; i <= number; i++){
 			factorial *= i;
 		}
 		printf("El resultado de !%d es: %d \n\n", number, factorial);
-		return 0;
+		returnValue = 0;
 	}
-	return -1;
+	return returnValue;
 }
 
-//Esto es sólo visible al scope del source file que lo contiene.
-//Esta función
-static int
